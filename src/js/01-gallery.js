285b0f3b7d4676c133825fca9e41636abb034c49
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-08-23 11:53:37 +0300

    fix styles for list

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const galleryListItems = createGalleryListHTML(galleryItems);
+
+galleryList.style.listStyle = 'none';
+
+galleryList.insertAdjacentHTML('beforeend', galleryListItems);
+
+function createGalleryListHTML(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
