ccd4d5664fcf8aa7fe42d281ca55d6aa320b6046
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-08-27 20:48:18 +0300

    change logic in video.js and feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  const stringifiedData = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, stringifiedData);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+  console.log(parsedData);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function fillFormWithSavedData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(savedData);
+
+  if (parsedData) {
+    refs.textarea.value = parsedData.message || '';
+    refs.input.value = parsedData.email || '';
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+  }
+}
+fillFormWithSavedData();
